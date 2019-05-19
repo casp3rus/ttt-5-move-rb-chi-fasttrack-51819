b9023f3ca5cb1de5ca17e7b431842d1695b4b4c8
Adrian Popan <adrian.popan@gmail.com>
2019-05-19 13:35:23 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,8 +6,8 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(position - 1)
-  
+def input_to_index(position)
+  board[position - 1] = "X"
 end
 
 def move
