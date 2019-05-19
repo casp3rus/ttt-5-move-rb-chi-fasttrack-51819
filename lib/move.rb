b085ac3cbd6ca624eece0f33a3f9e0d45ed72ce8
Adrian Popan <adrian.popan@gmail.com>
2019-05-19 13:42:04 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -5,8 +5,9 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
 def input_to_index(position)
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   i = position.to_i - 1
   board[i] = "X"
 end
